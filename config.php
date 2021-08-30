efca7b0dd2c1772798b46473a9d95f9c7e73686f
u3206769 <alex.catherine@live.com>
2021-08-30 10:45:17 +1000

    changes

--- config.php
+++ config.php
@@ -3,9 +3,17 @@
 $host       = "localhost";
 $username   = "root";
 $password   = "root";
-$dbname     = "test"; 
+$dbname     = "ben_test"; 
 $dsn        = "mysql:host=$host;dbname=$dbname"; 
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
+              
+/* Attempt to connect to MySQL database */
+try{
+    $pdo_connection = new PDO($dsn, $username, $password, $options);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
 ?>
